f30b3a472e1b236c385d5720452c27f9d6eb319f
KHLLS <kahlil122333@gmail.com>
2026-02-06 20:40:01 +0700

    update grafik

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import plotly.express as px
 import joblib
 
-st.set_page_config(page_title="Weather AI", layout="wide")
+st.set_page_config(page_title="Weather Prediction", layout="wide")
 
 def load_data():
     df = pd.read_csv('dataset/for_trained_weatherAUS.csv')
@@ -14,52 +14,90 @@ def load_data():
 def load_model():
     model = joblib.load('model/rain_prediction_model.pkl')
     scaler = joblib.load('model/scaler_weather.pkl')
-    return model, scaler
+    packet = joblib.load('model/rain_prediction_model_accuracy.pkl')
+    return model, scaler, packet
 
 df, df_c = load_data()
-model, scaler = load_model()
+model, scaler, packet = load_model()
+
+model = packet['model_obj']
+acc_score = packet['accuracy']
 
 month_map = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
 df['MonthName'] = df['Month'].map(month_map)
 list_kota = sorted(df_c['Location'].unique())
 
+presentage = df_c["RainTomorrow"].map({'Yes':'Rain','No':'Bright'})
+prob = presentage.value_counts()
+
+st.sidebar.info(f"Algorithm: Random Forest - Model Accuracy: {acc_score * 100:.2f}%")
+st.sidebar.markdown("---")
 st.sidebar.title("Navigation Wheater")
 menu = st.sidebar.radio("Menu", ["Dashboard", "Prediction"])
 
 if menu == "Dashboard":
     st.title("Weather Dashboard")
-    st.title("Summary")
+    st.title("Summary And Probability of Rain in Australia")
     
     c1, c2, c3 = st.columns(3)
     c1.metric("Total City", df_c['Location'].nunique())
     c2.metric("Total Rows", f"{len(df):,}")
     c3.metric("Total Columns", len(df.columns))
+    st.markdown("---")
+
+    st.subheader("Probability of Rain in Australia")
+    st.plotly_chart(px.pie(prob, values=prob.values, names=prob.index,title='Probability of Rain',height=500,))
+    st.write("Insight")
+    st.info("Historical data shows Australia is a dry continent. Rain only occurs on ~22%")
+    st.markdown("---")
 
     st.subheader("Correlation Between Columns")
     cols = ['MinTemp', 'MaxTemp', 'Rainfall', 'WindGustSpeed', 'Humidity9am', 'Humidity3pm', 'Pressure3pm', 'RainToday', 'RainTomorrow']
     corr = df[cols].corr()
-    st.plotly_chart(px.imshow(corr, text_auto=True, color_continuous_scale='RdBu_r'), use_container_width=True)
+    st.plotly_chart(px.imshow(corr, text_auto='.2f',height=600, color_continuous_scale='YlGnBu'), use_container_width=True)
+    st.markdown("---")
 
     st.subheader("Most Influential Column")
-    imp = pd.DataFrame({'feat': model.feature_names_in_, 'imp': model.feature_importances_}).sort_values('imp', ascending=False)
-    st.plotly_chart(px.bar(imp.head(10), x='imp', y='feat', orientation='h'), use_container_width=True)
+    imp = pd.DataFrame({'feature': model.feature_names_in_, 'importance': model.feature_importances_}).sort_values('importance', ascending=False)
+    st.plotly_chart(px.bar(imp.head(10), x='importance', y='feature', orientation='h'), use_container_width=True)
+    st.markdown("---")
 
     st.subheader("Analysis by City")
     kota = st.selectbox("Select", list_kota)
     df_kota = df[df['Location'] == list_kota.index(kota)]
-    
-    col_a, col_b = st.columns(2)
-    avg_r = df_kota['Rainfall'].mean()
-    col_a.info(f"Avarage Rain in {kota}: {avg_r:.2f} mm")
-    
-    rainy_month = df_kota.groupby('MonthName')['Rainfall'].mean().sort_values(ascending=False).index[0]
-    col_b.success(f"Wettest Month: {rainy_month}")
-    
-    month_rain = df_kota.groupby('MonthName')['Rainfall'].mean().reset_index()
-    st.plotly_chart(px.line(month_rain, x='MonthName', y='Rainfall', title=f"Monthly Rain Trend - {kota}"), use_container_width=True)
+    st.markdown("---")
+    tab1, tab2 = st.tabs(['Rainfall','Temperature'])
+    with tab1:
+        col_a, col_b = st.columns(2)
+        avg_r = df_kota['Rainfall'].mean()
+        col_a.info(f"Avarage Rain in {kota}: {avg_r:.2f} mm")
+        rainy_month = df_kota.groupby('MonthName')['Rainfall'].mean().sort_values(ascending=False).index[0]
+        col_b.success(f"Wettest Month: {rainy_month}")
+        month_rain = df_kota.groupby('MonthName')['Rainfall'].mean().reset_index()
+        st.plotly_chart(px.line(
+            month_rain, 
+            x='MonthName', 
+            y='Rainfall', 
+            title=f"Monthly Rain Trend - {kota}"
+            ), use_container_width=True)
+    with tab2:
+        col1, col2 = st.columns(2)
+        avg_t = df_kota['MaxTemp'].mean()
+        col1.info(f"Avarage Temp in {kota}: {avg_t:.2f} C")
+        hot_month = df_kota.groupby('MonthName')['MaxTemp'].max().sort_values(ascending=False).index[0]
+        col2.success(f"Hottest Temp in {kota}: {hot_month}")
+        month_temp = df_kota.groupby('MonthName')['MaxTemp'].mean().reset_index()
+        st.plotly_chart(px.line(
+            month_temp, 
+            x='MonthName', 
+            y='MaxTemp', 
+            title=f"Monthly Temp Trend - {kota}"
+            ), use_container_width=True)
+
 
 else:
     st.title("Rain Prediction for Tomorrow")
+    st.info("Fill in the weather data below to predict rain tomorrow")
     
     c1, c2, c3 = st.columns(3)
     with c1:
@@ -73,15 +111,15 @@ else:
     with c3:
         rain_now = st.number_input("Rainfall Today", value=0.0)
         wind = st.number_input("Wind Gust Speed", value=30.0)
-        today = st.radio("Raining Today?", [0, 1], format_func=lambda x: "Yes" if x==1 else "No")
+        # today = st.radio("Raining Today?", [0, 1], format_func=lambda x: "Yes" if x==1 else "No")
         bln = st.selectbox("Month", range(1, 13))
 
     if st.button("Prediction", use_container_width=True):
         loc_id = list_kota.index(pk_kota)
         
         input_raw = pd.DataFrame([[
-            loc_id, min_t, max_t, np.log1p(rain_now), np.log1p(wind), h9, h3, p3, today, bln
-        ]], columns=['Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'WindGustSpeed', 'Humidity9am', 'Humidity3pm', 'Pressure3pm', 'RainToday', 'Month'])
+            loc_id, min_t, max_t, np.log1p(rain_now), np.log1p(wind), h9, h3, p3, bln
+        ]], columns=['Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'WindGustSpeed', 'Humidity9am', 'Humidity3pm', 'Pressure3pm', 'Month'])
 
         for c in model.feature_names_in_:
             if c not in input_raw.columns: input_raw[c] = 0
@@ -91,7 +129,8 @@ else:
         
         prob = model.predict_proba(input_sc)[0][1]
         
-        if prob >= 0.4:
-            st.error(f"Result: Rain (Probability: {prob*100:.1f}%)")
+        if prob >= 0.35:
+            st.error(f"Result: Rain {prob * 100:.1f}%")
         else:
-            st.success(f"Result: Bright (Probability: {prob*100:.1f}%)")
+            st.success(f"Result: Bright {prob * 100:.1f}%")
+
